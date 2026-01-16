8ee0c1bc73e9787e38e01e6aca5b616e640cc0ad
GabeCurran <gabecurran01@gmail.com>
2026-01-16 17:57:15 -0500

    small mb addition to contact info

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
   <footer id="contact" className="flex flex-col items-center justify-center mt-12 gap-4">
-      <div className="flex items-center justify-center gap-6">
+      <div className="flex items-center justify-center gap-6 mb-8 sm:mb-0">
         <a target="_blank" rel="noreferrer" href="https://github.com/GabeCurran/" aria-label="GitHub">
           <div className="h-12 w-12 sm:h-16 sm:w-16">
             <Image alt="GitHub" src="/img/githubLight64.png" width={64} height={64} className="transition-transform hover:scale-110" />
